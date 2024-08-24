21aeeeedc43072eabcde82bfdec5ce1961555afc
Neha Panda <nehap2@unc.edu>
2024-08-24 10:41:07 -0400

    Finished exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,12 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730767309"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="Campers")
+greet(name="Yummy")
